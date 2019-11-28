d516819400dfdfaf1476fb3448fd604a1666ae84
Ubuntu <ubuntu@ip-172-31-20-220.ap-southeast-1.compute.internal>
2019-11-28 02:11:39 +0000

    Initial

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Import the Welcome function from ./Welcome.js
+import {Welcome} from './components/Welcome'
+
+// Do a default import from ./HeroImage.js
+import HeroImage from './components/HeroImage'
+
+// Do a default import from ./Alert.js
+import Alert from './components/Alert'
+
+// Do a default import from ./Reserve.js
+import Reserve from './components/Reserve'
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Welcome/>
+      <HeroImage/>
+      <Alert message="Please remember our discount vouchers expire end of Dec 2019!"/>
+      <Reserve/>
     </div>
   );
 }
